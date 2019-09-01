143c5666290e86d6dfc384d307848e27d3632521
laityts <laityts@gmail.com>
2019-09-01 23:16:56 +0800

    Add template

---
+++ template/anykernel.sh
@@ -0,0 +1,71 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Soviet-Star by LaKard0 @ 4dpa
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=1
+device.name1=tiffany
+device.name2=MI 5X
+supported.versions=9
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/platform/soc/7824900.sdhci/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
+
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# sepolicy
+$bin/magiskpolicy --load sepolicy --save sepolicy \
+  "allow init rootfs file execute_no_trans" \
+;
+
+# If the kernel image and dtbs are separated in the zip
+decompressed_image=/tmp/anykernel/kernel/Image
+compressed_image=$decompressed_image.gz
+if [ -f $compressed_image ]; then
+  # Hexpatch the kernel if Magisk is installed ('skip_initramfs' -> 'want_initramfs')
+  if [ -d $ramdisk/.backup -o -d $ramdisk/.magisk ]; then
+    ui_print " "; ui_print "Magisk detected! Patching kernel so reflashing Magisk is not necessary...";
+    $bin/magiskboot --decompress $compressed_image $decompressed_image;
+    $bin/magiskboot --hexpatch $decompressed_image 736B69705F696E697472616D667300 77616E745F696E697472616D667300;
+    $bin/magiskboot --compress=gzip $decompressed_image $compressed_image;
+  fi;
+
+  ui_print " "; ui_print "Checking for Project Treble..."; ui_print " ";
+  if [ "$(file_getprop /system/build.prop ro.treble.enabled)" = "true" ]; then
+    ui_print "Treble Status: Supported";
+    dtb=/tmp/anykernel/dtb-treble;
+  else
+    ui_print "Treble Status: Not supported";
+    dtb=/tmp/anykernel/dtb-nontreble;
+  fi;
+  
+  # Concatenate all of the dtbs to the kernel
+  cat $compressed_image $dtb/*.dtb > /tmp/anykernel/Image.gz-dtb;
+fi;
+
+# end ramdisk changes
+
+write_boot;
+## end install
